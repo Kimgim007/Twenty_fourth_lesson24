4207d43ac198883db3cf301a2db13e089b9118b6
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-07-08 11:47:34 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Twenty_fourth_lesson24
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ WebDriver/Driver.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+namespace Twenty_fourth_lesson24.WebDriver
+{
+    internal class Driver
+    {
+        private static IWebDriver? driver;
+        private static WebDriverWait? wait;
+
+        public static IWebDriver GetDriver()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+            }
+            return driver;
+        }
+        public static WebDriverWait SetDriver(IWebDriver driver, double time)
+        {
+            if (wait == null)
+            {
+                wait = new WebDriverWait(driver,TimeSpan.FromSeconds(time));
+            }
+            return wait;
+        }
+
+        public static void TeareDown()
+        {
+            driver.Quit();
+            driver = null;
+            wait = null;
+        }
+    }
+}
